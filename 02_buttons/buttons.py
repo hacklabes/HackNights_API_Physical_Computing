76d3fe1e8140f115fcaf044aa6442e27740016fc
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-02-09 22:30:53 +0000

    python button script

---
+++ 02_buttons/buttons.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gpiozero import LED, Button
+from signal import pause
+
+# set up 2 buttons using physical pull_down resistors and 0.01 seconds of "debouncing"
+yButton = Button(6,  pull_up=False, bounce_time=0.05)
+rButton = Button(19, pull_up=False, bounce_time=0.05)
+
+# set up 2 pins for LEDs
+yLed = LED(20)
+rLed = LED(21)
+
+# define behavior for rButton
+rButton.when_pressed = rLed.on
+rButton.when_released = rLed.off
+
+# define behavior for yButton
+yButton.when_pressed = yLed.on
+yButton.when_released = yLed.off
+
+# pause programme until killing it with ctrl-c
+pause()
